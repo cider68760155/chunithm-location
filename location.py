532041203ad1794783268800272fc5dbdba73bbe
cider68760155 <38486547+cider68760155@users.noreply.github.com>
2020-03-12 17:33:32 +0900

    bug fix on locaiton.py

--- location.py
+++ location.py
@@ -1,6 +1,7 @@
 import csv
 from bs4 import BeautifulSoup
 import requests
+import time
 name = []
 address = []
 for pref in range(47):
@@ -10,6 +11,8 @@ for pref in range(47):
     name.extend([a.text for a in soup.find_all(class_='store_name')])
     address.extend([a.text for a in soup.find_all(class_='store_address')])
     assert len(name) == len(address), 'len(name)!=len(address)'
-with open('location.csv', 'w', encoding='utf-8') as f:
+    time.sleep(0.5)
+
+with open('location.csv', 'w', encoding='utf-8', newline='') as f:
     writer = csv.writer(f)
     writer.writerows([[name[i], address[i]] for i in range(len(name))])
