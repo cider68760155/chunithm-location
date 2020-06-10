941f623b788e89323a6796d7896c0f3d626ca765
cider68760155 <38486547+cider68760155@users.noreply.github.com>
2020-06-10 21:58:02 +0900

    csv上での並び順を都道府県ごとに住所でソートに変更

--- location.py
+++ location.py
@@ -2,17 +2,19 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 import time
-name = []
-address = []
+store_all = []
 for pref in range(47):
     r = requests.get(
         "https://location.am-all.net/alm/location?gm=58&ct=1000&at="+str(pref))
     soup = BeautifulSoup(r.text, 'html.parser')
-    name.extend([a.text for a in soup.find_all(class_='store_name')])
-    address.extend([a.text for a in soup.find_all(class_='store_address')])
+    name = [a.text for a in soup.find_all(class_='store_name')]
+    address = [a.text for a in soup.find_all(class_='store_address')]
     assert len(name) == len(address), 'len(name)!=len(address)'
+    store = [[name[i], address[i]] for i in range(len(name))]
+    store.sort(key=lambda x: x[1])
+    store_all.extend(store)
     time.sleep(0.5)
 
 with open('location.csv', 'w', encoding='utf-8', newline='') as f:
     writer = csv.writer(f)
-    writer.writerows([[name[i], address[i]] for i in range(len(name))])
+    writer.writerows(store_all)
